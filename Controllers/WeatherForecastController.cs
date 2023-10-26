f6dfc21cfe530270881e9dadc2994d3b824fcbf8
sunil kumar <SUNIL19892317@gmail.com>
2023-10-26 23:30:59 +0530

    worked on api

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-
+using MtgApiManager.Lib.Core;
+using MtgApiManager.Lib.Model;
+using MtgApiManager.Lib.Service;
 namespace MyApi.Controllers;
 
 [ApiController]
@@ -16,17 +18,57 @@ public class WeatherForecastController : ControllerBase
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    //[HttpGet(Name = "GetWeatherForecast")]
+    //public IEnumerable<WeatherForecast> Get()
+    //{
+    //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    //    {
+    //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+    //        TemperatureC = Random.Shared.Next(-20, 55),
+    //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    //    })
+    //    .ToArray();
+    //}
+
+    /// <summary>
+    /// Api to get the Card Details
+    /// </summary>
+    /// <param name="Names"></param>
+    /// <param name="Colors"></param>
+    /// <param name="Types"></param>
+    /// <returns></returns>
+    [Route("GetCardList")]
+    [HttpPost]
+    public async Task<IActionResult> GetCardList(string Names, string Colors, string Types)
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        try
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            IMtgServiceProvider serviceProvider = new MtgServiceProvider();
+
+            ICardService service = serviceProvider.GetCardService();
+            //IOperationResult<List<ICard>> result1 = await service.AllAsync();
+
+            IOperationResult<List<ICard>> result = await service.Where(x => x.Name, Names)
+                      .Where(x => x.Colors, Colors)
+                      .Where(x => x.Types, Types)
+                      .AllAsync();
+
+            if (result.IsSuccess)
+            {
+                var value = result.Value;
+            }
+            else
+            {
+                var exception = result.Exception;
+            }
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return Ok(Convert.ToString(ex.Message));
+        }
     }
 }
